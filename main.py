1de185751ef71b945b71f769608d8989520f609d
Lucas Nascimento <nascimento.ti@gmail.com>
2011-10-16 23:57:59 -0200

    Refatorando e criando métodos de serviço

--- main.py
+++ main.py
@@ -3,87 +3,30 @@ import wsgiref.handlers
 import pyamf
 
 from google.appengine.ext import db
-from google.appengine.ext.db import polymodel
 from pyamf.remoting.gateway.wsgi import WSGIGateway
-from aetypes import Enum
+from model import TipoPrato
+from model import Cardapio
+from model import Prato
+from model import Destinario
+from model import Restaurante
+from model import DestinatarioRestaurante
 
-
-class DiaCardapio(Enum):
-	domingo = "domingo"
-	segunda = "segunda"
-	terca = "terca"
-	quarta = "quarta"
-	quinta = "quinta"
-	sexta = "sexta"
-	sabado = "sabado"
-	unico = "unico"
-	variado = "variado"
-
-class TipoPrato(db.Model):
-	nome = db.StringProperty()
 	
-class Prato(db.Model):
-	tipoPrato = db.ReferenceProperty(TipoPrato)
-	nome = db.StringProperty()
-	
-class Cardapio(polymodel.PolyModel):
-	nome = db.StringProperty()
-	descricao = db.StringProperty()
-	pratos = db.ListProperty(db.Key)
+def salvarRestaurante(restaurante):
+	restaurante.put()
+	return restaurante
 
-class CardapioConfig(Cardapio):
-	dia_semana = db.StringProperty()
+def recuperarRestaurantePorNome(nome):
+	return Restaurante.all().filter(" nome = ", nome).get()
 
-class Destinario(db.Model):
-	email = db.ListProperty(db.Email)
-	contaFacebook = db.StringProperty()
-	contaTwitter = db.StringProperty()	
-	
-class Restaurante(db.Model):
-	nomeFantasia = db.StringProperty()
-	conta_facebook = db.StringProperty()
-	conta_twitter = db.StringProperty()
-	tipo_cardapio = db.StringProperty()
-	email_cabecalho = db.TextProperty()
-	email_rodape = db.TextProperty()
-	cardapios = db.ListProperty(db.Key)
-	tipo_pratos = db.ListProperty(db.Key)
-	pratos = db.ListProperty(db.Key)
-	
-class DestinatarioRestaurante(db.Model):
-	destinatario = db.Reference(Destinario, collection_name = 'destinatario_restaurante')	
-	restaurante = db.Reference(Restaurante, collection_name = 'destinatario_restaurante')
-
-class Mensagem(db.Model):
-	data_envio = db.DateTimeProperty()
-	restaurante = db.ReferenceProperty(Restaurante, collection_name = "mensagem")
-	cardapio = db.ReferenceProperty(Cardapio, collection_name = "mensagem")
-	destinatarios = db.ListProperty(db.Key)
-	body = db.TextProperty()
-
-''' 
-Métodos de Serviço
-'''
-	
-def saveCategoria(newCategoria):
-	newCategoria.put()
-	return newCategoria
-
-def deleteCategoria(categoriaKey):
-	db.delete(categoriaKey)
-	
-def getCategorias():
-	return TipoPrato.all().fetch(100)	
-	
-
-'''
-Infra
-'''
+def recuperarRestaurantePorKey(key):
+	return db.get(key)
 	
 def main():
 	services = {
-		'getCategorias': getCategorias,
-		'saveCategoria': saveCategoria,
+		'salvarRestaurante': salvarRestaurante,
+		'recuperarRestaurantePorNome': recuperarRestaurantePorNome,
+		'recuperarRestaurantePorKey': recuperarRestaurantePorKey,
     }
 	
 	gateway = WSGIGateway(services, logger=logging, debug=True)
