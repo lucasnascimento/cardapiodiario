d64e8baf93c0bc7db8cad5ccf271c1d676aa7588
Lucas Nascimento <lucas.nascimento@pearson.com>
2012-10-01 20:30:16 -0300

    Criando projeto RAILS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cardapiodiario::Application.config.secret_token = '6e0d1f5005a7dd1f9881ec7def8dbdd7246e97654da297a16876e1a0a75e68cf01c7b47d846df208b3fcb0ed5cb2c38bb2ed2756eaca77821b15e83c2fd70910'
